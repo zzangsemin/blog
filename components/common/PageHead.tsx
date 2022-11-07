45c48641967420dd9849fe71ebeeb5d0f2b37651
zzangsemin <1015tpals@gmail.com>
2022-11-08 00:53:12 +0900

    Feat: openGraph 이미지 자동 생성 추가

--- components/common/PageHead.tsx
+++ components/common/PageHead.tsx
@@ -10,16 +10,17 @@ interface PageHeadProps{
     title?: string;
     description?: string;
     ogImageSrc?: string;
+    ogImageTitle?: string;
 }
 
-const PageHead = ({title, description, ogImageSrc}: PageHeadProps) => {
+const PageHead = ({title, description, ogImageSrc, ogImageTitle}: PageHeadProps) => {
     const {asPath} = useRouter();
 
     const siteUrl = process.env.SITE_URL;
 
     const fullUrl = `${siteUrl}${asPath}`;
     const fullTitle = title ? `${title} | ${DEFAULT_TITLE}` : DEFAULT_TITLE;
-    const fullOgImageSrc = ogImageSrc ?? `${siteUrl}${DEFAULT_OG_IMAGE_SRC}`;
+    const fullOgImageSrc = ogImageSrc ?? `${siteUrl}${DEFAULT_OG_IMAGE_SRC}${ogImageTitle ? `?title=${ogImageTitle}` : ''}`;
 
     return (
         <Head>
