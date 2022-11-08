e153cf0a60f312406e11f6cea8ea27e7038d7f63
zzangsemin <1015tpals@gmail.com>
2022-11-09 00:26:46 +0900

    Style: 디자인 개선

--- components/layout/HeaderMenu.tsx
+++ components/layout/HeaderMenu.tsx
@@ -36,7 +36,7 @@ const HeaderMenu = ({isMenuOpen}: HeaderMenuProps) => {
             className={`z-50 bg-white fixed top-0 bottom-0 w-3/5 max-w-sm transition-all duration-500 ${isMenuOpen ? 'left-0' : '-left-[60%]'}`}
         >
             <div className='py-8 flex flex-col h-full'>
-                <div className="relative w-full h-1/3">
+                <div className="relative w-full h-1/4 mx-auto">
                     <Image src={idk} alt="Profile Image" objectFit="contain" layout="fill"   />
                 </div>
                 <h1 className="text-center font-bold text-2xl">
@@ -45,11 +45,11 @@ const HeaderMenu = ({isMenuOpen}: HeaderMenuProps) => {
                     </Link>
                 </h1>
 
-                <ul className="mt-8 flex flex-col">
+                <ul className="mt-8 flex flex-col text-gray-500">
                     {Object.entries(NavTable).map(([href, value]) => (
-                        <li key={href} className={`text-2xl text-black/70 hover:text-black hover:bg-gray-100 ${asPath === href ? "text-black bg-gray-100" : ""}`}>
+                        <li key={href} className={`text-xl hover:text-black hover:bg-gray-100 ${asPath === href ? "text-black bg-gray-100" : ""}`}>
                             <Link href={href}>
-                                <a className='flex flex-row gap-2 items-center p-4'>
+                                <a className='flex flex-row gap-2 items-center px-8 py-6'>
                                     <span>{value.icon}</span>
                                     {value.name}
                                 </a>
