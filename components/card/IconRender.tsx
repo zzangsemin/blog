47ba26cc440812c23aaa10cb3a311d44a9770d8a
zzangsemin <1015tpals@gmail.com>
2022-11-13 03:44:38 +0900

    Feat: icon & tag list 추가

---
+++ components/card/IconRender.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+import React from 'react'
+import { CardData } from 'types/types'
+
+interface IconRenderProps{
+    icon: CardData['icon'];
+}
+
+const IconRender = ({icon}: IconRenderProps) => {
+    if(!icon) return null;
+
+    if(icon.type === 'emoji') return <span>{icon.emoji}</span>
+
+    return (
+        <span className='mr-2 align-middle'>
+            <Image src={icon.type === "external" ? icon.external.url : icon.file.url} alt='icon' width={24} height={24} className='rounded-full overflow-hidden' /> 
+        </span>
+    )
+}
+
+export default IconRender
